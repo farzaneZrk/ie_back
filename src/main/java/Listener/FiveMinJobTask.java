d75c5ec3ffa2f365e7183e87610e8ec04d7cfb06
sina kachoei <sina95kachoei@gmail.com>
2019-05-02 13:18:24 +0430

    fiveMinTask is added

---
+++ src/main/java/Listener/FiveMinJobTask.java
@@ -0,0 +1,22 @@
+package Listener;
+
+import Model.ProjectRepo;
+import Model.SkillRepo;
+
+import java.io.IOException;
+
+public class FiveMinJobTask implements Runnable {
+    @Override
+    public void run() {
+        // Do your hourly job here.
+        System.out.println("Job trigged by scheduler");
+        String skillsJson = null;
+        try {
+            ProjectRepo.setUpProjectlist();
+            SkillRepo.setUpSkillList();
+            System.out.println("get initial data successfully");
+        } catch (IOException e) {
+            System.out.println("!!!!can't connect to server 142.93.134.194:8000 to get data!!!!");
+        }
+    }
+}
--- src/main/java/Listener/MyListener.java
+++ src/main/java/Listener/MyListener.java
@@ -11,10 +11,14 @@ import javax.servlet.http.HttpSessionBindingEvent;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @WebListener()
 public class MyListener implements ServletContextListener,
         HttpSessionListener, HttpSessionAttributeListener {
+    private ScheduledExecutorService scheduler;
 
     // Public constructor is required by servlet spec
     public MyListener() {
@@ -26,24 +30,22 @@ public class MyListener implements ServletContextListener,
     // -------------------------------------------------------
     public void contextInitialized(ServletContextEvent sce) {
       /* This method is called when the servlet context is
-         initialized(when the Web application is deployed). 
+         initialized(when the Web application is deployed).
          You can initialize servlet context related data here.
       */
-        String skillsJson = null;
-        try {
-            ProjectRepo.setUpProjectlist();
-            SkillRepo.setUpSkillList();
-            System.out.println("get initial data successfully");
-        } catch (IOException e) {
-            System.out.println("!!!!can't connect to server 142.93.134.194:8000 to get data!!!!");
-        }
+        scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(new FiveMinJobTask(), 0, 5, TimeUnit.MINUTES);
+//        scheduler.scheduleAtFixedRate(new FiveMinJobTask(), 0, 1, TimeUnit.SECONDS);
+
     }
 
     public void contextDestroyed(ServletContextEvent sce) {
-      /* This method is invoked when the Servlet Context 
-         (the Web application) is undeployed or 
+      /* This method is invoked when the Servlet Context
+         (the Web application) is undeployed or
          Application Server shuts down.
       */
+        scheduler.shutdownNow();
+
     }
 
     // -------------------------------------------------------
